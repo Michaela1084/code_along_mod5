f3cffdfd31fb239a13325593db6ca5572edcf811
Michaela1084 <142283872+Michaela1084@users.noreply.github.com>
2023-12-17 19:27:15 -0800

    First commit

---
+++ config.py
@@ -0,0 +1,26 @@
+import os 
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+dotenv_path = os.path.join(basedir, '.env')
+load_dotenv(dotenv_path)
+
+class Config():
+    '''
+        Set config variables for the flask app
+        using Enviroment variables where available.
+        Otherwise, create the config variable if not done already.   
+    '''
+
+    FLASK_APP = os.getenv('FLASK_APP')
+    FLASK_ENV = os.getenv('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Ryan will never get access to my CSS'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_NOTIFACTIONS = False 
+    SQLALCHEMY_TRACK_MODIFACTIONS = False 
+    
+
+
+
+
+    # FINAL ARANGEMENT!!!! 
